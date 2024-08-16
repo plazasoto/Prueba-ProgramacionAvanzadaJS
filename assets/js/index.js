d9429c716313c567030d5f1a63970a0f751b585a
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-08-16 13:05:51 -0400

    Creando instancias con formulario.

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,7 +35,7 @@ class Leon extends Animal{
         super(nombre, edad, img, comentarios, sonido);
     }
 
-    rugir(){}
+    rugir(){console.log("Hola, soy " + this._nombre)}
 }
 
 class Lobo extends Animal{
@@ -43,7 +43,7 @@ class Lobo extends Animal{
         super(nombre, edad, img, comentarios, sonido);
     }
 
-    aullar(){}
+    aullar(){console.log("Hola, soy " + this._nombre)}
 }
 
 class Oso extends Animal{
@@ -51,7 +51,7 @@ class Oso extends Animal{
         super(nombre, edad, img, comentarios, sonido);
     }
 
-    gruñir(){}
+    gruñir(){console.log("Hola, soy " + this._nombre)}
 }
 
 class Serpiente extends Animal{
@@ -59,7 +59,7 @@ class Serpiente extends Animal{
         super(nombre, edad, img, comentarios, sonido);
     }
 
-    sisear(){}
+    sisear(){console.log("Hola, soy " + this._nombre)}
 }
 
 class Aguila extends Animal{
@@ -67,5 +67,59 @@ class Aguila extends Animal{
         super(nombre, edad, img, comentarios, sonido);
     }
 
-    chillar(){console.log("COOOOOOOO")}
+    chillar(){console.log("Hola, soy " + this._nombre)}
+}
+
+// Función para crear instancia correspondiente a nombre de animal
+const crearAnimal = (nombre, edad, img, comentarios, sonido) =>{
+    switch(nombre){
+        case "Leon":
+            return new Leon(nombre, edad, img, comentarios, sonido);
+        case "Lobo":
+            return new Lobo(nombre, edad, img, comentarios, sonido);
+        case "Oso":
+            return new Oso(nombre, edad, img, comentarios, sonido);
+        case "Serpiente":
+            return new Serpiente(nombre, edad, img, comentarios, sonido);
+        case "Aguila":
+            return new Aguila(nombre, edad, img, comentarios, sonido);
+        default:
+            return new Animal(nombre, edad, img, comentarios, sonido);
+    }
+}
+
+const boton = document.getElementById("btnRegistrar");
+boton.addEventListener("click", (event) => {
+
+    // Datos para construir animal
+    let nombreAnimal = document.getElementById("animal").value;
+    let edadAnimal = document.getElementById("edad").value;
+    let imagenAnimal;//se obtendrá de json
+    let comentarioAnimal = document.getElementById("comentarios").value;
+    let sonidoAnimal;//se obtendrá de json
+    
+    // Instancia
+    let animal;//se inicializa luego de obtener datos del json
+    
+    // Consulta a animales.jason
+    let requestAnimales = request("./animales.json");
+
+    requestAnimales.then(async (val)=>{       
+        await val.animales.forEach(element => {
+            // Obteniendo imagen y sonido del animal
+            if(element.name == nombreAnimal){
+                imagenAnimal = element.imagen;
+                sonidoAnimal = element.sonido;
+                animal = crearAnimal(nombreAnimal, edadAnimal, imagenAnimal, comentarioAnimal, sonidoAnimal);
+                //console.log(animal);
+            }
+        });
+    }); 
+})
+
+// Consulta asincrónica
+const request = async (url) => {
+    const results = await fetch(url);
+    const response = await results.json();
+    return response;
 }
